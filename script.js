d6e9e1fd2e35fbb87eb8127eebcd86d975828355
jainakshay-28 <jainakshay3597@gmail.com>
2026-06-21 11:06:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log("Portfolio Loaded");
+const text = [
+    "Oracle Cloud Engineer",
+    "OCI Administrator",
+    "Oracle Database Specialist",
+    "Linux Administrator"
+];
+
+let i = 0;
+let j = 0;
+let current = "";
+let isDeleting = false;
+
+function type() {
+
+    current = text[i];
+
+    if (!isDeleting) {
+        document.getElementById("typing").innerHTML =
+            current.substring(0, j++);
+    }
+    else {
+        document.getElementById("typing").innerHTML =
+            current.substring(0, j--);
+    }
+
+    if (j == current.length + 1)
+        isDeleting = true;
+
+    if (j == 0 && isDeleting) {
+        isDeleting = false;
+        i++;
+
+        if (i == text.length)
+            i = 0;
+    }
+
+    setTimeout(type, 100);
+}
+
+type();
